b06df9351db82c19c3d9e3fe81accfbb815437a3
Andrew Brown <drusepth@gmail.com>
2021-03-31 13:55:15 -0700

    bundle material icons with pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -14,7 +14,12 @@ WickedPdf.config = {
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+
+  # Heroku location
   exe_path: '/app/vendor/bundle/bin/wkhtmltopdf'
+
+  # dev location
+  # exe_path: '/usr/bin/wkhtmltopdf'
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
