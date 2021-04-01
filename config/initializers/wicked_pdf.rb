2aabb2d33362e6e628554d3a910b7fe9c962fdfc
Andrew Brown <drusepth@gmail.com>
2021-03-31 18:42:20 -0700

    use sass to inject pipeline paths for materialicon fonts

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -16,10 +16,10 @@ WickedPdf.config = {
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
   # Heroku location
-  exe_path: '/app/vendor/bundle/bin/wkhtmltopdf'
+  # exe_path: '/app/vendor/bundle/bin/wkhtmltopdf'
 
   # dev location
-  # exe_path: '/usr/bin/wkhtmltopdf'
+  exe_path: '/usr/bin/wkhtmltopdf'
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
