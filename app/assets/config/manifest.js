7587022df125348458d6e311e049c1acf1a378e0
Andrew Brown <drusepth@gmail.com>
2021-04-01 01:14:11 -0700

    add open source footer

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,2 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_directory ../stylesheets .ttf
-//= link_directory ../stylesheets .woff
-//= link_directory ../stylesheets .woff2
